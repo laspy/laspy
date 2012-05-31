c6451f9339d864e121366340e02e882b77b6df63
Grant Brown <grant.brown73@gmail.com>
2012-05-31 15:17:56 -0500

    flesh out actual header data methods in base

--- laspy/base.py
+++ laspy/base.py
@@ -364,14 +364,13 @@ class FileManager():
             raise LaspyException("Dimension: " + str(name) + 
                             "not found.")
 
-    def _get_datum(self, rec_offs, dim):
-        pass
-    
-    def get_datum(self, name):
-        pass
+    def _get_datum(self, rec_offs, spec):
+        raw_data = self._map[(rec_offs + spec.offs):(rec_offs + spec.offs + spec.length)]
+        return(struct.unpack(spec.fmt, raw_data))
 
     def get_header_property(self, name):
-        pass
+        spec = self.header_format[name]
+        return(self._get_datum(0, spec))
 
     def _get_dimension(self,offs, fmt, length, raw = False):
         if type(self.point_refs) == bool:
