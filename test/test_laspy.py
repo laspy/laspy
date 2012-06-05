7dbb22164e72607c4581062d921b02c92034a518
Howard Butler <hobu.inc@gmail.com>
2012-06-05 13:19:27 -0500

    make sure to close in tearDown

--- test/test_laspy.py
+++ test/test_laspy.py
@@ -438,6 +438,7 @@ class LasHeaderWriterTestCase(unittest.TestCase):
         s2 = self.FileObject.header.get_scale()
         self.assertTrue(s1 == s2)
     def tearDown(self):
+        self.FileObject.close()
         os.remove(self.tempfile)
 
 class LasWriteModeTestCase(unittest.TestCase):
@@ -480,6 +481,7 @@ class LasWriteModeTestCase(unittest.TestCase):
         self.assertTrue(all(Z == File2.get_z()))
 
     def tearDown(self):
+        self.FileObject.close()
         os.remove(self.tempfile)
 
 def test_laspy():
