4933e7e0b5ab5472d5bd438098c5a044c419d912
Grant Brown <grant.brown73@gmail.com>
2012-07-02 10:09:07 -0500

    refactor gigantic format constructor into type specific functions

--- laspy/util.py
+++ laspy/util.py
@@ -77,6 +77,7 @@ class Format():
         '''Build the :obj:`laspy.util.Format` instance. '''
         fmt = str(fmt)
         self.fmt = fmt
+        self.overwritable = overwritable
         try:
             self._etree = etree.Element("Format")
         except:
@@ -89,8 +90,95 @@ class Format():
         if not (fmt in ("0", "1", "2", "3", "4", "5","6","7","8","9","10", "VLR", "EVLR", "h1.0", "h1.1", "h1.2", "h1.3", "h1.4")):
             raise LaspyException("Invalid format: " + str(fmt))
         ## Point Fields
-        if fmt in ("0", "1", "2", "3", "4", "5"):
+        if fmt in ([str(x) for x in range(11)]):
             self.format_type = "point format = " + fmt
+            self.build_point_format(fmt)
+        ## VLR Fields
+        if fmt == "VLR":
+            self.format_type = "VLR"
+            self.build_vlr_format(fmt)
+        if fmt == "EVLR":
+            self.format_type = "EVLR"
+            self.build_evlr_format(fmt)
+        ## Header Fields
+        if fmt[0] == "h": 
+            self.build_header(fmt)
+        ## Shared 
+        self.build_extra_bytes(extra_bytes)
+        
+    def build_extra_bytes(self, extra_bytes):
+        if not extra_bytes in (0, False): 
+            self.add("extra_bytes", ctypes.c_ubyte, extra_bytes) 
+        self.lookup = {}
+        for spec in self.specs:
+            self.lookup[spec.name] = spec
+        self.packer = Struct(self.pt_fmt_long)
+
+    def build_header(self, fmt):
+        self.format_type = "header version = " + fmt[1:]
+        self.add("file_sig",ctypes.c_char, 4, pack = True, overwritable=self.overwritable)
+        self.add("file_source_id", ctypes.c_ushort, 1)
+        self.add("global_encoding",ctypes.c_ushort, 1)
+        self.add("proj_id_1",ctypes.c_ulong, 1)
+        self.add("proj_id_2", ctypes.c_ushort, 1)
+        self.add("proj_id_3", ctypes.c_ushort, 1)
+        self.add("proj_id_4", ctypes.c_ubyte, 8)
+        self.add("version_major", ctypes.c_ubyte, 1, overwritable=self.overwritable)
+        self.add("version_minor", ctypes.c_ubyte, 1, overwritable=self.overwritable)
+        self.add("system_id", ctypes.c_char, 32, pack=True)
+        self.add("software_id",  ctypes.c_char, 32, pack = True)
+        self.add("created_day", ctypes.c_ushort, 1)
+        self.add("created_year", ctypes.c_ushort,1)
+        self.add("header_size", ctypes.c_ushort, 1, overwritable=self.overwritable)
+        self.add("data_offset", ctypes.c_ulong, 1)
+        self.add("num_variable_len_recs",  ctypes.c_ulong, 1)
+        self.add("data_format_id",  ctypes.c_ubyte, 1, overwritable=self.overwritable)
+        self.add("data_record_length",  ctypes.c_ushort, 1)
+        if fmt != "h1.4":
+            self.add("point_records_count", ctypes.c_ulong, 1)         
+        else:
+            self.add("legacy_point_records_count", ctypes.c_ulong, 1)
+            self.add("legacy_point_return_count", ctypes.c_ulong, 5) 
+        if fmt in ("h1.0", "h1.1", "h1.2", "h1.3"):
+            self.add("point_return_count", ctypes.c_long, 5)
+
+        self.add("x_scale", ctypes.c_double, 1)
+        self.add("y_scale", ctypes.c_double, 1)
+        self.add("z_scale", ctypes.c_double, 1)
+        self.add("x_offset", ctypes.c_double, 1)
+        self.add("y_offset", ctypes.c_double, 1)
+        self.add("z_offset", ctypes.c_double, 1) 
+        self.add("x_max", ctypes.c_double, 1)
+        self.add("x_min", ctypes.c_double, 1)
+        self.add("y_max",ctypes.c_double, 1)
+        self.add("y_min",ctypes.c_double, 1)
+        self.add("z_max", ctypes.c_double, 1)
+        self.add("z_min", ctypes.c_double, 1)
+        if fmt in ("h1.3", "h1.4"):
+            self.add("start_wavefm_data_rec", ctypes.c_ulonglong, 1)
+        if fmt == "h1.4":
+            self.add("start_first_evlr", ctypes.c_ulonglong, 1)
+            self.add("num_evlrs", ctypes.c_ulong, 1)
+            self.add("point_records_count", ctypes.c_ulonglong, 1)
+            self.add("point_return_count", ctypes.c_ulonglong, 15)
+                
+
+    def build_evlr_format(self, fmt):
+        self.add("reserved", ctypes.c_ushort, 1)
+        self.add("user_id", ctypes.c_char, 16)
+        self.add("record_id", ctypes.c_ushort, 1)
+        self.add("rec_len_after_header", ctypes.c_ulonglong, 1)
+        self.add("description", ctypes.c_char, 32, pack = True)
+
+    def build_vlr_format(self, fmt):
+        self.add("reserved", ctypes.c_ushort, 1)
+        self.add("user_id", ctypes.c_char, 16)
+        self.add("record_id", ctypes.c_ushort, 1)
+        self.add("rec_len_after_header", ctypes.c_ushort, 1)
+        self.add("description", ctypes.c_char, 32, pack = True)
+
+    def build_point_format(self, fmt):
+        if fmt in [str(x) for x in range(6)]:
             self.add("X", ctypes.c_long, 1)
             self.add("Y", ctypes.c_long, 1)
             self.add("Z", ctypes.c_long, 1)
@@ -100,32 +188,32 @@ class Format():
             self.add("scan_angle_rank", ctypes.c_ubyte, 1)
             self.add("user_data",  ctypes.c_ubyte, 1)
             self.add("pt_src_id",  ctypes.c_ushort, 1)
-        if fmt in ("1", "3", "4", "5"):
-            self.add("gps_time", ctypes.c_double, 1)
-        if fmt in ("3", "5"):
-            self.add("red", ctypes.c_ushort, 1)
-            self.add("green", ctypes.c_ushort, 1)
-            self.add("blue" , ctypes.c_ushort,1)
-        elif fmt == "2":
-            self.add("red", ctypes.c_ushort, 1)
-            self.add("green", ctypes.c_ushort, 1)
-            self.add("blue" , ctypes.c_ushort,1)
-        if fmt == "4":
-            self.add("wave_packet_desc_index", ctypes.c_ubyte, 1)
-            self.add("byte_offset_to_waveform_data", ctypes.c_ulonglong,1)
-            self.add("waveform_packet_size",ctypes.c_long, 1)
-            self.add("return_point_waveform_loc",  ctypes.c_float, 1)
-            self.add("x_t", ctypes.c_float, 1)
-            self.add("y_t", ctypes.c_float, 1)           
-            self.add("z_t", ctypes.c_float, 1)
-        elif fmt == "5":
-            self.add("wave_packet_desc_index", ctypes.c_ubyte, 1)
-            self.add("byte_offset_to_waveform_data", ctypes.c_ulonglong,1)
-            self.add("wavefm_pkt_size", ctypes.c_ulong, 1)
-            self.add("return_point_waveform_loc", ctypes.c_float, 1)
-            self.add("x_t", ctypes.c_float, 1)
-            self.add("y_t", ctypes.c_float, 1)          
-            self.add("z_t", ctypes.c_float, 1)
+            if fmt in ("1", "3", "4", "5"):
+                self.add("gps_time", ctypes.c_double, 1)
+            if fmt in ("3", "5"):
+                self.add("red", ctypes.c_ushort, 1)
+                self.add("green", ctypes.c_ushort, 1)
+                self.add("blue" , ctypes.c_ushort,1)
+            elif fmt == "2":
+                self.add("red", ctypes.c_ushort, 1)
+                self.add("green", ctypes.c_ushort, 1)
+                self.add("blue" , ctypes.c_ushort,1)
+            if fmt == "4":
+                self.add("wave_packet_desc_index", ctypes.c_ubyte, 1)
+                self.add("byte_offset_to_waveform_data", ctypes.c_ulonglong,1)
+                self.add("waveform_packet_size",ctypes.c_long, 1)
+                self.add("return_point_waveform_loc",  ctypes.c_float, 1)
+                self.add("x_t", ctypes.c_float, 1)
+                self.add("y_t", ctypes.c_float, 1)           
+                self.add("z_t", ctypes.c_float, 1)
+            elif fmt == "5":
+                self.add("wave_packet_desc_index", ctypes.c_ubyte, 1)
+                self.add("byte_offset_to_waveform_data", ctypes.c_ulonglong,1)
+                self.add("wavefm_pkt_size", ctypes.c_ulong, 1)
+                self.add("return_point_waveform_loc", ctypes.c_float, 1)
+                self.add("x_t", ctypes.c_float, 1)
+                self.add("y_t", ctypes.c_float, 1)          
+                self.add("z_t", ctypes.c_float, 1)
         elif fmt in ("6", "7", "8", "9", "10"):
             self.add("X", ctypes.c_long, 1)
             self.add("Y", ctypes.c_long, 1)
@@ -152,82 +240,7 @@ class Format():
             self.add("x_t", ctypes.c_float, 1)
             self.add("y_t", ctypes.c_float, 1)          
             self.add("z_t", ctypes.c_float, 1)
-        ## VLR Fields
-        if fmt == "VLR":
-            self.format_type = "VLR"
-            self.add("reserved", ctypes.c_ushort, 1)
-            self.add("user_id", ctypes.c_char, 16)
-            self.add("record_id", ctypes.c_ushort, 1)
-            self.add("rec_len_after_header", ctypes.c_ushort, 1)
-            self.add("description", ctypes.c_char, 32, pack = True)
 
-        if fmt == "EVLR":
-            self.format_type = "EVLR"
-            self.add("reserved", ctypes.c_ushort, 1)
-            self.add("user_id", ctypes.c_char, 16)
-            self.add("record_id", ctypes.c_ushort, 1)
-            self.add("rec_len_after_header", ctypes.c_ulonglong, 1)
-            self.add("description", ctypes.c_char, 32, pack = True)
-
-        ## Header Fields
-        if fmt[0] == "h": 
-            self.format_type = "header version = " + fmt[1:]
-            self.add("file_sig",ctypes.c_char, 4, pack = True, overwritable=overwritable)
-            self.add("file_source_id", ctypes.c_ushort, 1)
-            self.add("global_encoding",ctypes.c_ushort, 1)
-            self.add("proj_id_1",ctypes.c_ulong, 1)
-            self.add("proj_id_2", ctypes.c_ushort, 1)
-            self.add("proj_id_3", ctypes.c_ushort, 1)
-            self.add("proj_id_4", ctypes.c_ubyte, 8)
-            self.add("version_major", ctypes.c_ubyte, 1, overwritable=overwritable)
-            self.add("version_minor", ctypes.c_ubyte, 1, overwritable=overwritable)
-            self.add("system_id", ctypes.c_char, 32, pack=True)
-            self.add("software_id",  ctypes.c_char, 32, pack = True)
-            self.add("created_day", ctypes.c_ushort, 1)
-            self.add("created_year", ctypes.c_ushort,1)
-            self.add("header_size", ctypes.c_ushort, 1, overwritable=overwritable)
-            self.add("data_offset", ctypes.c_ulong, 1)
-            self.add("num_variable_len_recs",  ctypes.c_ulong, 1)
-            self.add("data_format_id",  ctypes.c_ubyte, 1, overwritable=overwritable)
-            self.add("data_record_length",  ctypes.c_ushort, 1)
-            if fmt != "h1.4":
-                self.add("point_records_count", ctypes.c_ulong, 1)         
-            else:
-                self.add("legacy_point_records_count", ctypes.c_ulong, 1)
-                self.add("legacy_point_return_count", ctypes.c_ulong, 5) 
-            if fmt in ("h1.0", "h1.1", "h1.2", "h1.3"):
-                self.add("point_return_count", ctypes.c_long, 5)
-
-            self.add("x_scale", ctypes.c_double, 1)
-            self.add("y_scale", ctypes.c_double, 1)
-            self.add("z_scale", ctypes.c_double, 1)
-            self.add("x_offset", ctypes.c_double, 1)
-            self.add("y_offset", ctypes.c_double, 1)
-            self.add("z_offset", ctypes.c_double, 1) 
-            self.add("x_max", ctypes.c_double, 1)
-            self.add("x_min", ctypes.c_double, 1)
-            self.add("y_max",ctypes.c_double, 1)
-            self.add("y_min",ctypes.c_double, 1)
-            self.add("z_max", ctypes.c_double, 1)
-            self.add("z_min", ctypes.c_double, 1)
-            if fmt in ("h1.3", "h1.4"):
-                self.add("start_wavefm_data_rec", ctypes.c_ulonglong, 1)
-            if fmt == "h1.4":
-                self.add("start_first_evlr", ctypes.c_ulonglong, 1)
-                self.add("num_evlrs", ctypes.c_ulong, 1)
-                self.add("point_records_count", ctypes.c_ulonglong, 1)
-                self.add("point_return_count", ctypes.c_ulonglong, 15)
-                
-
-        ## Shared 
-
-        if not extra_bytes in (0, False): 
-            self.add("extra_bytes", ctypes.c_ubyte, extra_bytes) 
-        self.lookup = {}
-        for spec in self.specs:
-            self.lookup[spec.name] = spec
-        self.packer = Struct(self.pt_fmt_long)
-        
     def add(self, name, fmt, num, pack = False, overwritable = True):
         if len(self.specs) == 0:
             offs = 0
