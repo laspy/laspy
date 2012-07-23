263bb3d9f84b36377c42fce6bc835d9a10d7b488
Grant Brown <grant.brown73@gmail.com>
2012-07-23 10:58:49 -0500

    Move tools into laspy/tools, build and install them with setup.py.

---
+++ laspy/tools/lascopy.py
@@ -0,0 +1,177 @@
+from laspy.file import File
+from laspy.util import Format
+import argparse
+
+
+class lascopy():
+    def __init__(self):
+        self.parse_args()
+        self.copy_data()
+
+    def parse_args(self):
+        '''Set up the argparser to accept command line arguments.'''
+        parser = argparse.ArgumentParser(
+                description="""Accept in_file and out_file .LAS files, and 
+                convert from in_file to out_file according to point_format
+                and file_version.""")
+        parser.add_argument('in_file', metavar='in_file', type=str, 
+                            nargs='+', help='input file path')
+        parser.add_argument('out_file', metavar='out_file', type=str, 
+                            nargs='+', help='output file path')
+        parser.add_argument('point_format', metavar='point_format', type=int, 
+                            nargs='+', help='output point format (0-10)')
+        parser.add_argument('file_version', metavar='file_version', type=str, 
+                            nargs='+', help='output file version 1.0-1.4')
+        parser.add_argument('-u', type=bool,
+                help="Update the header histogram? (slow)", default=False)
+        parser.add_argument('-b', type=bool,
+                help="Attempt to preserve incompatable sub-byte sized data fields if present? (slow)", 
+                default=False)
+
+        self.args = parser.parse_args()
+
+    def copy_data(self):
+        ## Set several global flags to determine lascopy behavior
+        UPDATE_HISTOGRAM= self.args.u
+        PRESERVE = self.args.b
+
+        ## Try to open in_file in read mode. 
+        file_version = self.args.file_version[0]
+        point_format = self.args.point_format[0]
+        try:
+            inFile = File(self.args.in_file[0], mode = "r")
+        except Exception, error:
+            print("There was an error reading in the input file: ")
+            print(error)
+            quit()
+
+        ## Verify that the user has supplied a compatable point_format and file_version
+        if not (point_format in range(11)):
+            raise Exception("Invalid point format: %i" % point_format)
+        if (point_format > 5 and file_version !="1.4"):
+            raise Exception("Point formats 6-10 are only supported by version 1.4 files.")
+        if (point_format > 3 and not (file_version in ["1.3", "1.4"])):
+            raise Exception("Point format %i is not available for file version %s" %
+                            (point_format, file_version))
+        if (point_format >= 2 and not (file_version in ["1.2", "1.3", "1.4"])):
+            raise Exception("Point format %i is not available for file version %s" % 
+                            (point_format, file_version))
+
+        ## Store depricated data for use later.
+        old_file_version = inFile.header.version
+        old_point_format = inFile.header.data_format_id
+
+
+        ## Set global flag, which indicates whether the input and Output point formats 
+        ## have compatable sub-byte fields. 
+        SUB_BYTE_COMPATABLE = (old_point_format <= 5) == (point_format <= 5)
+
+        ## Tell the user what we're doing. 
+        print("Input File: " + self.args.in_file[0] + ", %i point records." % len(inFile))
+        print("Output File: " + self.args.out_file[0])
+        print("Converting from file version %s to version %s." %(old_file_version, file_version)) 
+        print("Converting from point format %i to format %i."%(old_point_format, point_format))
+
+        ## Warn the user if they chose potentially incompatable point formats and didn't specify -b=True
+        if (not SUB_BYTE_COMPATABLE) and (not PRESERVE):
+            print("""WARNING: The sub-byte sized fields differ between point formats 
+                    %i and %i. By default this information will be lost. If you want 
+                    laspy to try to preserve as much as possible, specify -b=True, though 
+                    this may be quite slow depending on the size of the dataset.""" 
+                    % (old_point_format, point_format))
+
+
+        ## Build the new header, check if we need to do anythin special. This includes:
+        ## 1. Check if there are extra dimensions defined which we need to copy.
+        ## 2. Check if we need to get rid of some/all EVLRs for the output file_version
+        ## 3. See if we need to re-map legacy fields for 1.4 files.
+        try:
+            new_header = inFile.header.copy()
+            new_header.format = file_version 
+            new_header.data_format_id = point_format
+
+            old_data_rec_len = new_header.data_record_length
+            old_std_rec_len = Format(old_point_format).rec_len
+            diff =   old_data_rec_len - old_std_rec_len
+            if (diff > 0):
+                print("Extra Bytes Detected.")
+
+            new_header.data_record_length = Format(point_format).rec_len + ((diff > 0)*diff)
+            evlrs = inFile.header.evlrs
+            if file_version != "1.4" and old_file_version == "1.4":
+                print("Warning: input file has version 1.4, and output file does not. This may cause trunctation of header data.")
+                new_header.point_return_count = inFile.header.legacy_point_return_count
+                new_header.point_records_count = inFile.header.legacy_point_records_count
+            if not (file_version in ["1.3", "1.4"]) and old_file_version in ["1.3", "1.4"]:
+                print("Stripping any EVLRs")
+                evlrs = []
+            if (file_version == "1.3" and len(inFile.header.evlrs) > 1):
+                print("Too many EVLRs for format 1.3, keeping the first one.")
+                evlrs = inFile.header.evlrs[0]
+            outFile = File(self.args.out_file[0], header = new_header, mode = "w", vlrs = inFile.header.vlrs, evlrs = evlrs)
+            if outFile.point_format.rec_len != outFile.header.data_record_length:
+                pass
+        except Exception, error:
+            print("There was an error instantiating the output file.")
+            print(error)
+            quit()
+
+        ## Copy point dimensions. 
+        try:
+            for dimension in inFile.point_format.specs:
+                if dimension.name in outFile.point_format.lookup:
+                    ## Skip sub_byte field record bytes if incompatable
+                    if (not SUB_BYTE_COMPATABLE and dimension.name in ("raw_classification", 
+                        "classification_flags", "classification_byte", "flag_byte")):
+                        continue
+                    outFile.writer.set_dimension(dimension.name, inFile.reader.get_dimension(dimension.name))
+                    print("Copying: " + dimension.name)
+            ## Do we need to do anything special for sub-byte fields?
+            if (not SUB_BYTE_COMPATABLE and PRESERVE):
+                # Are we converting down or up?
+                print("Attepmpting to copy sub-byte fields (this might take awhile)")
+                up = old_point_format < point_format
+                if up:
+                    outFile.classification = inFile.classification
+                    outFile.return_num = inFile.return_num
+                    outFile.num_returns = inFile.num_returns
+                    outFile.scan_dir_flag = inFile.scan_dir_flag
+                    outFile.edge_flight_line = inFile.edge_flight_line
+                    outFile.synthetic = inFile.synthetic
+                    outFile.withheld = inFile.withheld
+                    outFile.key_point = inFile.key_point
+                else:
+                    try:
+                        outFile.classification = inFile.classification
+                    except Exception, error:
+                        print("Error, couldnt set classification.")
+                        print(error)
+                    try:
+                        outFile.return_num = inFile.return_num
+                    except Exception, error:
+                        print("Error, coun't set return number.")
+                        print(error)
+                    try:
+                        outFile.num_returns = inFile.num_returns
+                    except:
+                        print("Error, couldn't set number of returns.")
+                    outFile.scan_dir_flag = inFile.scan_dir_flag
+                    outFile.edge_flight_line = inFile.edge_flight_line
+                    outFile.synthetic = inFile.synthetic
+                    outFile.key_point = inFile.key_point
+                    outFile.withheld = inFile.withheld
+
+
+        except Exception, error:
+            print("Error copying data.")
+            print(error)
+            quit()
+        ## Close the files, and optionally update new file histogram.
+        inFile.close()
+        outFile.close(ignore_header_changes = not UPDATE_HISTOGRAM)
+
+
+
+def main():
+    lc = lascopy()
+    print("Copy Complete")
---
+++ laspy/tools/lasverify.py
@@ -0,0 +1,206 @@
+from laspy import file as File
+import argparse
+
+class lasverify():
+    def __init__(self):
+        self.parse_args()
+        self.verify()
+        ## Setup argparse to accept command line arguments
+    def parse_args(self):
+        parser = argparse.ArgumentParser(
+                description='''Compare LAS files file_1 and file_2 by dimension, header
+                       field, vlr and evlr.''')
+        parser.add_argument('file_1', metavar='file_1', type=str, nargs='+',
+                            help='LAS file 1 path.')
+        parser.add_argument('file_2', metavar='file_2', type=str, nargs='+',
+                            help='LAS file 2 path.')
+
+        parser.add_argument('-b', type=bool,help='''Attempt to compare incompatable 
+                sub-byte sized data fields if present? (slow)''', default=False)
+
+        self.args = parser.parse_args()
+
+    def verify(self):
+    ## Set global varibles for convenience.
+        file_1 = self.args.file_1[0]
+        file_2 = self.args.file_2[0]
+        PRESERVE = self.args.b 
+
+    ## Try to open both files in read mode.
+        try:
+            inFile1 = File.File(file_1,mode= "r")
+            inFile2 = File.File(file_2,mode= "r")
+        except Exception, error:
+            print("Error reading in files:")
+            print(error)
+            quit()
+
+    ## Set global flag to indicate whether we need to look at incompatable 
+    ## bit field bytes. 
+        SUB_BYTE_COMPATABLE = ((inFile1.header.data_format_id <= 5) == 
+                            (inFile2.header.data_format_id <= 5)) 
+
+    ## Warn the user if they chose not to check incompatable point formats.
+        if (not SUB_BYTE_COMPATABLE) and (not PRESERVE):
+            print("""WARNING: Point formats %i and %i have mismatched sub-byte fields. 
+                    The default behavior in this case is to ignore these fields during 
+                    the file_verify procedure. If you want laspy to attempt to match up
+                    sub-byte data between these two formats, specify -b=True 
+                    (this might take some time depending on the size of the file)""")
+
+        def print_title(string):
+            print("#"*65)
+            print("#  " + string + " "*(65 - (len(string) + 4)) + "#")
+            print("#"*65)
+
+    ## Define convenience function to try to compare point dimensions. 
+        def f(x):
+            try:
+                return(1*(list(inFile1.reader.get_dimension(x)) == list(inFile2.reader.get_dimension(x))))
+            except:
+                outstr = "Dimension: %s" %x
+                outstr += " "*(50-len(outstr))
+                if not (x in inFile1.point_format.lookup) and (x in inFile2.point_format.lookup):
+                    print(outstr + "not present in file_1.")
+                elif not (x in inFile2.point_format.lookup) and (x in inFile1.point_format.lookup):
+                    print(outstr + "not present in file_2.")
+                else:
+                    print("There was an error comparing dimension: %s" + str(x))
+                return(2)
+
+    ## Define convenience function to try to compare header fields. 
+        def g(x):
+            try:
+                return((1*(inFile1.reader.get_header_property(x) == inFile2.reader.get_header_property(x))))
+            except:
+                outstr = "Header Property: %s" % x
+                outstr += (" "*(50-len(outstr)))
+                if not (x in inFile1.header.header_format.lookup) and (x in inFile2.header.header_format.lookup):
+                    print(outstr + "not present in file_1.")
+                elif not (x in inFile2.header.header_format.lookup) and (x in inFile1.header.header_format.lookup):
+                    print(outstr + "not present in file_2.")
+                else:
+                    print("There was an error while comparing header property: %s" + str(x))
+                return(2)
+
+    ## Build union fo header fields, then check them.
+        print_title("Checking Headers")
+        try:
+            passed = 0
+            failed = 0
+            header_props = set()
+            for item in inFile1.reader.header_format.specs:
+                header_props.add(item.name)
+            for item in inFile2.reader.header_format.specs:
+                header_props.add(item.name)
+            for item in header_props:
+                outstr = "Header Property: %s" % item
+                outstr += " "*(50-len(outstr))
+                result = g(item)
+                if result == 1:
+                    print(outstr + "identical")
+                    passed += 1
+                elif not result == 2:
+                    print(outstr + "different")
+                    print("   File 1: " + str(inFile1.reader.get_header_property(item)))
+                    print("   File 2: " + str(inFile2.reader.get_header_property(item)))
+                    failed += 1
+            print("%i of %i header fields match." % (passed, passed + failed))
+        except:
+            print("There was an error while comparing headers. ")
+
+    ## Check VLRs
+        print_title("Checking VLRs")
+
+        try:
+            if len(inFile1.header.vlrs) != len(inFile2.header.vlrs):
+                print("Number of VLRs differs: file_1 has %i and file_2 has %i. Comparing where possible..."
+                        % (len(inFile1.header.vlrs), len(inFile2.header.vlrs)))
+            for i in xrange(min(len(inFile1.header.vlrs), len(inFile2.header.vlrs))):
+                outstr = "VLR Record: %i" %i
+                outstr += " "*(50 - len(outstr))
+                vlr1 = inFile1.header.vlrs[i].to_byte_string()
+                vlr2 = inFile2.header.vlrs[i].to_byte_string()
+                if vlr1 == vlr2:
+                    print(outstr + "identical")
+                else:
+                    print(outstr + "different")
+        except:
+            print("There was a problem comparing VLRs")
+
+    ## Check EVLRs
+        print_title("Checking EVLRs")
+        try:
+            if len(inFile1.header.evlrs) != len(inFile2.header.evlrs):
+                print("Number of VLRs differs: file_1 has %i and file_2 has %i. Comparing where possible..."
+                        % (len(inFile1.header.evlrs), len(inFile2.header.evlrs)))
+
+            for i in xrange(min(len(inFile1.header.evlrs), len(inFile2.header.evlrs))):
+                outstr = "EVLR Record: %i" %i
+                outstr += " "*(50 - len(outstr))
+                vlr1 = inFile1.header.evlrs[i].to_byte_string()
+                vlr2 = inFile1.header.evlrs[i].to_byte_string()
+                if vlr1 == vlr2:
+                    print(outstr + "identical")
+                else:
+                    print(outstr + "different")
+        except:
+            print("There was a problem comparing EVLRs")
+
+    ## Build a union of dimensions in each file, then compare them.
+        dims = set()
+        for item in inFile1.reader.point_format.lookup.keys():
+            dims.add(item)
+        for item in inFile2.reader.point_format.lookup.keys():
+            dims.add(item)
+        print_title("Checking Dimensions")
+        passed = 0
+        failed = 0
+        for dim in dims:
+            if not SUB_BYTE_COMPATABLE and dim in ("raw_classification","classification_flags", "classification_byte", "flag_byte"):
+                continue
+            outstr = "Dimension: %s" % dim 
+            outstr += " "*(50-len(outstr))
+            result = f(dim)
+            if result == 1:
+                passed += 1
+                print(outstr +  "identical")
+            elif result != 2:
+                failed += 1
+                print(outstr + "different")
+
+        def print_sb(string, result):
+            outstr  ="Sub-byte field: " + string
+            outstr += " "*(50-len(outstr))
+            if result:
+                print(outstr + "identical")
+                return(1)
+            else:
+                print(outstr + "different")
+                return(0)
+
+    ## If we need to, compare valid sub-byte fields
+        sb_total = 0
+        if not SUB_BYTE_COMPATABLE and PRESERVE:
+            print("Comparing sub-byte fields (this might take awhile)")
+            sb_total += print_sb("classification", all(inFile1.classification == inFile2.classification)) 
+            sb_total += print_sb("return_num", all(inFile1.return_num == inFile2.return_num))
+            sb_total += print_sb("num_returns", all(inFile1.num_returns == inFile2.num_returns))
+            sb_total += print_sb("scan_dir_flag", all(inFile1.scan_dir_flag == inFile2.scan_dir_flag))
+            sb_total += print_sb("edge_flight_line", all(inFile1.edge_flight_line == inFile2.edge_flight_line))
+            sb_total += print_sb("synthetic", all(inFile1.synthetic == inFile2.synthetic))
+            sb_total += print_sb("withheld", all(inFile1.withheld == inFile2.withheld))
+            sb_total += print_sb("key_point", all(inFile1.key_point == inFile2.key_point))
+
+
+    ## Print summary
+        print(str(passed) + " identical dimensions out of " + str(passed + failed))
+        if not SUB_BYTE_COMPATABLE and PRESERVE:
+            print("%i identical sub-byte fields out of %i" %(sb_total, 8))
+
+        inFile1.close()
+        inFile2.close()
+
+def main():
+    vf = lasverify()
+    print("Verify Complete")
