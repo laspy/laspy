b02b40900b5620972930cd0c201b4db1a6a69754
Grant Brown <grant.brown73@gmail.com>
2017-08-09 08:26:01 -0500

    Fix import for glviewer

--- laspy/file.py
+++ laspy/file.py
@@ -158,7 +158,7 @@ class File(object):
 
     def visualize(self, mode = "default", dim = "intensity"):
         try:
-            import glviewer
+            from laspy import glviewer
             glviewer.run_glviewer(self, mode= mode, dim = dim)
             return(0)
         except Exception as err:
