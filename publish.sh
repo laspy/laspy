6aa5956ff14d9eae94a982ba34e5e6591d6b217e
Kristian Evers <kristianevers@gmail.com>
2017-12-05 20:06:00 +0100

    Use twine in publishing procedure

--- publish.sh
+++ publish.sh
@@ -1,5 +1,4 @@
-python setup.py register
-python setup.py sdist --formats=gztar,zip upload
-python setup.py bdist_wheel upload
-python setup.py bdist_egg upload
-
+rm dist/*
+python3 setup.py bdist_wheel --universal
+python3 setup.py sdist --formats=gztar,zip
+twine upload dist/*
