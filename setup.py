3cd515cd7a2d2b8e16187f7f336aaf663fe4a324
Howard Butler <howard@hobu.co>
2020-09-04 08:09:39 -0500

    no lazperf on windows, flake8'ify setup.py

--- setup.py
+++ setup.py
@@ -1,41 +1,44 @@
-import shutil
 from setuptools import setup
-
 
 with open('README.md') as f:
     readme = f.read()
 
+__version__ = None
 # Get __version without importing
 with open('laspy/__init__.py', 'r') as fp:
     # get and exec just the line which looks like "__version__ = '0.9.4'"
     exec(next(line for line in fp if '__version__' in line))
 
-
-setup(name          = 'laspy',
-      version       = __version__,
-      description   = 'Native Python ASPRS LAS read/write library',
-      license       = 'BSD',
-      keywords      = 'gis lidar las',
-      author        = 'Grant Brown',
-      author_email  = 'grant.brown73@gmail.com',
-      url   = 'https://github.com/laspy/laspy',
-      long_description = readme,
-      long_description_content_type = 'text/markdown',
-      packages      = ['laspy', 'test','laspy.tools'],
-      install_requires = ['numpy'],
-      test_suite = 'test.test_laspy',
-      data_files = [("test/data", ["simple.las", "simple1_3.las", "simple1_4.las", "simple.laz"])],
-      include_package_data = True,
-      zip_safe = False,
-      entry_points = {'console_scripts':['lascopy = laspy.tools.lascopy:main',
-                                         'lasexplorer = laspy.tools.lasexplorer:main',
-                                         'lasnoise = laspy.tools.lasnoise:main',
-                                         'lasverify = laspy.tools.lasverify:main',
-                                         'lasvalidate = laspy.tools.lasvalidate:main',
-                                         'lasviewer = laspy.tools.lasviewer:main'
-                                        ]},
-
-      classifiers   = [
+setup(name='laspy',
+      version=__version__,
+      description='Native Python ASPRS LAS read/write library',
+      license='BSD',
+      keywords='gis lidar las',
+      author='Grant Brown',
+      author_email='grant.brown73@gmail.com',
+      url='https://github.com/laspy/laspy',
+      long_description=readme,
+      long_description_content_type='text/markdown',
+      packages=['laspy', 'laspy.tools'],
+      test_suite='test.test_laspy',
+      data_files=[("test/data",
+                  ["simple.las", "simple1_3.las",
+                   "simple1_4.las", "simple.laz"])],
+      install_requires=['numpy'],
+      extras_require={
+          "laz": 'lazperf ; platform_system!="Windows"'
+          },
+      include_package_data=True,
+      zip_safe=False,
+      entry_points={'console_scripts':
+                    ['lascopy = laspy.tools.lascopy:main',
+                     'lasexplorer = laspy.tools.lasexplorer:main',
+                     'lasnoise = laspy.tools.lasnoise:main',
+                     'lasverify = laspy.tools.lasverify:main',
+                     'lasvalidate = laspy.tools.lasvalidate:main',
+                     'lasviewer = laspy.tools.lasviewer:main'
+                     ]},
+      classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
@@ -44,5 +47,4 @@ setup(name          = 'laspy',
         'Programming Language :: Python',
         'Topic :: Scientific/Engineering :: GIS'
         ],
-)
-
+      )
