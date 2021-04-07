8cb7e8a45460b90f2eb8672fb166de39f6f00af2
tmontaigu <thomas.montaigu@laposte.net>
2021-04-07 22:24:06 +0200

    add the examples

---
+++ examples/field-randomizer.py
@@ -0,0 +1,50 @@
+from argparse import ArgumentParser
+from pathlib import Path
+
+import numpy as np
+
+import laspy
+
+
+def main(args):
+    files = [args.in_path] if args.in_path.is_file() else list(args.in_path.glob("*.la[s-z]"))
+
+    if args.out_path.suffix and len(files) > 1:
+        raise SystemExit("in_path is a directory and out path is a file")
+
+    for i, path in enumerate(files, start=1):
+        print(f"{i} / {len(files)} -> {path}")
+
+        las = laspy.read(path)
+
+        dimensions = (dim for dim in las.point_format.dimensions if dim.name not in args.exclude)
+        for dimension in dimensions:
+            print(f"\t{dimension.name}", end='')
+            if dimension.kind == laspy.DimensionKind.FloatingPoint:
+                print("...skipped because it is floating point")
+                continue
+
+            if np.any(las[dimension.name] != 0) and args.keep_existing:
+                print("...skipped because it is already populated")
+                continue
+
+            type_str = dimension.type_str() if dimension.kind != laspy.DimensionKind.BitField else 'u1'
+            las[dimension.name] = np.random.randint(dimension.min, dimension.max + 1, len(las.points), type_str)
+            print()
+
+        if args.out_path.suffix:
+            las.write(args.out_path)
+        else:
+            las.write(args.out_path / path.name)
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser('Randomize fields of your las file')
+    parser.add_argument('in_path', type=Path)
+    parser.add_argument('out_path', type=Path)
+    parser.add_argument('--keep-existing', action='store_true')
+    parser.add_argument('--exclude', nargs='*', default=["X", "Y", "Z"])
+
+    args = parser.parse_args()
+
+    main(args)
---
+++ examples/recursive-split.py
@@ -0,0 +1,90 @@
+import argparse
+import sys
+from pathlib import Path
+from typing import List
+from typing import Optional
+
+import numpy as np
+
+import laspy
+
+
+def recursive_split(x_min, y_min, x_max, y_max, max_x_size, max_y_size):
+    x_size = x_max - x_min
+    y_size = y_max - y_min
+
+    if x_size > max_x_size:
+        left = recursive_split(x_min, y_min, x_min + (x_size // 2), y_max, max_x_size, max_y_size)
+        right = recursive_split(x_min + (x_size // 2), y_min, x_max, y_max, max_x_size, max_y_size)
+        return left + right
+    elif y_size > max_y_size:
+        up = recursive_split(x_min, y_min, x_max, y_min + (y_size // 2), max_x_size, max_y_size)
+        down = recursive_split(x_min, y_min + (y_size // 2), x_max, y_max, max_x_size, max_y_size)
+        return up + down
+    else:
+        return [(x_min, y_min, x_max, y_max)]
+
+
+def tuple_size(string):
+    try:
+        return tuple(map(float, string.split("x")))
+    except:
+        raise ValueError("Size must be in the form of numberxnumber eg: 50.0x65.14")
+
+
+def main():
+    parser = argparse.ArgumentParser("LAS recursive splitter", description="Splits a las file bounds recursively")
+    parser.add_argument("input_file")
+    parser.add_argument("output_dir")
+    parser.add_argument("size", type=tuple_size, help="eg: 50x64.17")
+    parser.add_argument("--points-per-iter", default=10**6, type=int)
+
+    args = parser.parse_args()
+
+    with laspy.open(sys.argv[1]) as file:
+        sub_bounds = recursive_split(
+            file.header.x_min,
+            file.header.y_min,
+            file.header.x_max,
+            file.header.y_max,
+            args.size[0],
+            args.size[1]
+        )
+
+        writers: List[Optional[laspy.LasWriter]] = [None] * len(sub_bounds)
+        try:
+            count = 0
+            for points in file.chunk_iterator(args.points_per_iter):
+                print(f"{count / file.header.point_count * 100}%")
+
+                # For performance we need to use copy
+                # so that the underlying arrays are contiguous
+                x, y = points.x.copy(), points.y.copy()
+
+                point_piped = 0
+
+                for i, (x_min, y_min, x_max, y_max) in enumerate(sub_bounds):
+                    mask = (x >= x_min) & (x <= x_max) & (y >= y_min) & (y <= y_max)
+
+                    if np.any(mask):
+                        if writers[i] is None:
+                            output_path = Path(sys.argv[2]) / f"output_{i}.laz"
+                            writers[i] = laspy.open(output_path,
+                                                    mode='w',
+                                                    header=file.header)
+                        sub_points = points[mask]
+                        writers[i].write_points(sub_points)
+
+                    point_piped += np.sum(mask)
+                    if point_piped == len(points):
+                        break
+                count += len(points)
+            print(f"{count / file.header.point_count * 100}%")
+        finally:
+            for writer in writers:
+                if writer is not None:
+                    writer.close()
+
+
+if __name__ == '__main__':
+    main()
