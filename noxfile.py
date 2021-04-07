1267482e22ecadb11a89f510a23f97c1c46a8d69
tmontaigu <thomas.montaigu@laposte.net>
2021-04-07 22:23:47 +0200

    add noxfile.py

---
+++ noxfile.py
@@ -0,0 +1,15 @@
+import nox
+
+
+@nox.session(python=["3.6", "3.7", "3.8", "3.9"])
+@nox.parametrize("laz_backend", [None, "lazrs", "laszip"])
+def tests(session, laz_backend):
+    session.install("pytest")
+    if laz_backend is None:
+        session.install(".")
+    else:
+        session.install(f".[{laz_backend}]")
+    session.run("pytest", "-q", "pylastests")
+
+
+
