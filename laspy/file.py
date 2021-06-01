80e9122a02615639f97c1f55e078de93b35c9a3b
tmontaigu <thomas.montaigu@laposte.net>
2021-06-01 21:55:32 +0200

    link to the doc + tell how to stay on 1.x in the File class doc

--- laspy/file.py
+++ laspy/file.py
@@ -8,5 +8,6 @@ class File:
             To read a file do: las = laspy.read('somefile.laz')
             To create a new LAS data do: las = laspy.create(point_format=2, file_version='1.2')
             To write a file previously read or created: las.write('somepath.las')
-            See the documentation for more information about the changes"""
+            To stay on laspy 1.x: pip install laspy<2.0.0
+            See the documentation for more information about the changes https://laspy.readthedocs.io/en/latest/"""
         )
