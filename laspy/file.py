7118aaef51b120a6c83dfcf80366fa909506e2c9
tmontaigu <thomas.montaigu@laposte.net>
2021-06-15 18:45:25 +0200

    Update File error message to be clearer

--- laspy/file.py
+++ laspy/file.py
@@ -4,10 +4,13 @@ from . import errors
 class File:
     def __init__(self, *args, **kwargs) -> None:
         raise errors.LaspyException(
-            """laspy changed:
-            To read a file do: las = laspy.read('somefile.laz')
-            To create a new LAS data do: las = laspy.create(point_format=2, file_version='1.2')
-            To write a file previously read or created: las.write('somepath.las')
-            To stay on laspy 1.x: pip install laspy<2.0.0
+            """You are using laspy 2.0, which has several improvements over 1.x
+            but with several breaking changes.
+            To stay on laspy 1.x: `pip install laspy<2.0.0`
+            
+            In short:
+              - To read a file do: las = laspy.read('somefile.laz')
+              - To create a new LAS data do: las = laspy.create(point_format=2, file_version='1.2')
+              - To write a file previously read or created: las.write('somepath.las')
             See the documentation for more information about the changes https://laspy.readthedocs.io/en/latest/"""
         )
