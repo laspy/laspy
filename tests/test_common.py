laspy/laspy

commit 8426cbc2991f88d64dbde336904199126a989a4d

Improve assigning to multiple fields of point record
It will now be possible to assign to many fields
of a `LasData` at once:

```Python
las = laspy.read(file)

las[['x', 'y', 'z']] = np.ones(len(las), dtype='3f8')

las[['red', 'green', 'blue']] = np.ones((len(las), 3), dtype='u2')
```

---

Internal changes are:
  - `LasData` now uses a `ScaleAwarePointRecord` to share behaviour
    of (x , y, z scaled dimentions)
  - Implement `PackedPointRecord.__setitem__(key, value)` to handle
    the case when the key is a list/tuple of dimension name.

laspy/lasdata.py
@@ -2,6 +2,8 @@ import logging
 import pathlib
 from typing import Union, Optional, List, Sequence, overload, BinaryIO
 
+import numpy as np
+
 from . import errors
 from .compression import LazBackend
 from .header import LasHeader
@@ -29,54 +31,35 @@ class LasData:
     """
 
     def __init__(
-        self, header: LasHeader, points: Optional[record.PackedPointRecord] = None
+        self,
+        header: LasHeader,
+        points: Optional[
+            Union[record.PackedPointRecord, record.ScaleAwarePointRecord]
+        ] = None,
     ) -> None:
         if points is None:
-            points = record.PackedPointRecord.zeros(
-                header.point_count, header.point_format
+            points = record.ScaleAwarePointRecord.zeros(
+                header.point_count, header=header
             )
-        elif points.point_format != header.point_format:
+        if points.point_format != header.point_format:
             raise errors.LaspyException("Incompatible Point Formats")
+        if isinstance(points, record.PackedPointRecord):
+            points = record.ScaleAwarePointRecord(
+                points.array,
+                header.point_format,
+                scales=header.scales,
+                offsets=header.offsets,
+            )
+        else:
+            assert np.all(header.scales, points.scales)
+            assert np.all(header.offsets, points.offsets)
         self.__dict__["_points"] = points
-        self.points: record.PackedPointRecord
+        self.points: record.ScaleAwarePointRecord
         self.header: LasHeader = header
         if header.version.minor >= 4:
             self.evlrs: Optional[VLRList] = VLRList()
         else:
             self.evlrs: Optional[VLRList] = None
-
-    @property
-    def x(self) -> ScaledArrayView:
-        """Returns the scaled x positions of the points as doubles"""
-        return ScaledArrayView(self.X, self.header.x_scale, self.header.x_offset)
-
-    @x.setter
-    def x(self, value) -> None:
-        if len(value) > len(self.points):
-            self.points.resize(len(value))
-        self.x[:] = value
-
-    @property
-    def y(self) -> ScaledArrayView:
-        """Returns the scaled y positions of the points as doubles"""
-        return ScaledArrayView(self.Y, self.header.y_scale, self.header.y_offset)
-
-    @y.setter
-    def y(self, value) -> None:
-        if len(value) > len(self.points):
-            self.points.resize(len(value))
-        self.y[:] = value
-
-    @property
-    def z(self) -> ScaledArrayView:
-        """Returns the scaled z positions of the points as doubles"""
-        return ScaledArrayView(self.Z, self.header.z_scale, self.header.z_offset)
-
-    @z.setter
-    def z(self, value) -> None:
-        if len(value) > len(self.points):
-            self.points.resize(len(value))
-        self.z[:] = value
 
     @property
     def point_format(self) -> PointFormat:
@@ -245,12 +228,7 @@ class LasData:
         offsets: optional
                  New offsets to be used. If not provided, the offsets won't change.
         """
-        if scales is None:
-            scales = self.header.scales
-        if offsets is None:
-            offsets = self.header.offsets
-
-        record.apply_new_scaling(self, scales, offsets)
+        self.points.change_scaling(scales, offsets)
 
         self.header.scales = scales
         self.header.offsets = offsets

tests/test_common.py
@@ -182,7 +182,7 @@ def test_coords_when_using_create_from_header(las):
 
 
 def test_slicing(las):
-    las.points = las.points[len(las.points) // 2 :]
+    las.points = las.points[len(las.points) // 2:]
 
 
 @pytest.mark.parametrize("do_compress", do_compression)
@@ -207,3 +207,50 @@ def test_point_record_setitem_scaled_view():
 def test_laspy_file_raises():
     with pytest.raises(laspy.errors.LaspyException):
         laspy.file.File("some path")
+
+
+def test_lasdata_setitem_xyz_with_2d_array():
+    las = laspy.read(simple_las)
+
+    xyz = np.ones(len(las), dtype='3f8')
+    xyz[..., 1] = 2.0
+    xyz[..., 2] = 3.0
+
+    las[['x', 'y', 'z']] = xyz
+
+    assert np.all(las.x == xyz[..., 0])
+    assert np.all(las.y == xyz[..., 1])
+    assert np.all(las.z == xyz[..., 2])
+
+
+def test_lasdata_setitem_xyz_with_structured_array():
+    las = laspy.read(simple_las)
+
+    xyz = np.ones(len(las), dtype=[('x', 'f8'), ('y', 'f8'), ('z', 'f8')])
+    xyz['y'] = 2.0
+    xyz['z'] = 3.0
+
+    las[['x', 'y', 'z']] = xyz
+
+    assert np.all(las.x == xyz['x'])
+    assert np.all(las.y == xyz['y'])
+    assert np.all(las.z == xyz['z'])
+
+
+def test_lasdata_setitem_one_dimension():
+    las = laspy.read(simple_las)
+
+    las[['x']] = np.ones(len(las), 'f8') * 17.0
+    assert np.all(las.x == 17.0)
+
+
+def test_lasdata_setitem_works_with_subfields():
+    las = laspy.read(simple_las)
+
+    new_values = np.ones(len(las), dtype=[('classification', 'u1'), ('return_number', 'u1')])
+    new_values['classification'] = 23
+    new_values['return_number'] = 2
+
+    las[['classification', 'return_number']] = new_values
+    assert np.all(las.classification == 23)
+    assert np.all(las.return_number == 2)

laspy/lasreader.py
@@ -110,12 +110,8 @@ class LasReader:
     def read(self) -> LasData:
         """Reads all the points not read and returns a LasData object"""
         points = self.read_points(-1)
-        if not points:
-            points = record.PackedPointRecord.empty(self.header.point_format)
-        else:
-            points = record.PackedPointRecord(points.array, points.point_format)
-
         las_data = LasData(header=self.header, points=points)
+
         if self.header.version.minor >= 4:
             if (
                 self.header.are_points_compressed

laspy/point/record.py
@@ -133,7 +133,7 @@ class PackedPointRecord:
                 self.array, np.zeros(size_diff, dtype=self.array.dtype)
             )
         elif size_diff < 0:
-            self.array = self._array[:new_size].copy()
+            self.array = self.array[:new_size].copy()
 
     def _append_zeros_if_too_small(self, value):
         """Appends zeros to the points stored if the value we are trying to
@@ -187,6 +187,22 @@ class PackedPointRecord:
 
     def __setitem__(self, key, value):
         """Sets elements in the array"""
+        if isinstance(key, (tuple, list)):
+            if not isinstance(value, np.ndarray):
+                value = np.asarray(value)
+
+            if value.dtype.isbuiltin == 0:
+                # value is most likely a structured array (dtype = [('name1', 'type1'), ...])
+                # https://numpy.org/devdocs/reference/generated/numpy.dtype.isbuiltin.html
+                for name, v_name in zip(key, value.dtype.names):
+                    self[name] = value[v_name]
+            else:
+                if len(key) == 1 and value.ndim == 1:
+                    value = value[..., np.newaxis]
+                for i, name in enumerate(key):
+                    self[name] = value[..., i]
+            return
+
         self._append_zeros_if_too_small(value)
         if isinstance(key, str):
             self[key][:] = value
@@ -305,7 +321,13 @@ class ScaleAwarePointRecord(PackedPointRecord):
         self.offsets = offsets
 
     def __getitem__(self, item):
-        if isinstance(item, (slice, np.ndarray)):
+        if isinstance(item, (int, slice, np.ndarray, list, tuple)):
+            if isinstance(item, (list, tuple)):
+                # x, y ,z do not really exists in the array, but they are computed from X, Y, Z
+                item = [
+                    item if item not in ("x", "y", "z") else item.upper()
+                    for item in item
+                ]
             return ScaleAwarePointRecord(
                 self.array[item], self.point_format, self.scales, self.offsets
             )

commit c0bb69ff047d50765a0d7fe6e7cece3cce06630d

Fix las.x, las.y, las.z not setting X, Y or Z
This fixes the regression introduced by commit 8426cbc2991f88d64dbde336904199126a989a4d
where setting x, y or z like: `las.x = some_array` would not properly set `las.X` like it used to
and like it should.

When doing `las.x = ...` (or `.y`, or `.z`)
`___setattr__` was called, this would then call `super().__setattr__(key, value)`.

The commit 8426cbc2991f88d64dbde336904199126a989a4d removed the
x, y and z propeties (getter and setter), so `super().__setattr__(key, value)` would then create
a new attribute called `x` on the LasData instance instead of calling the `x` property setter
that handled the details that needs to be taken care of (setting the X values in the actual point record array).

We fix this by making the `___setattr__`  call to forward the call to the inner `ScaleAwarePointRecord`
so that the X, Y or Z values are actually set.

Fixes issue #189

laspy/lasdata.py
@@ -354,6 +354,7 @@ class LasData:
         if (
             key in self.point_format.dimension_names
             or key in self.points.array.dtype.names
+            or key in ("x", "y", "z")
         ):
             self.points[key] = value
         elif key in dims.DIMENSIONS_TO_TYPE:

tests/test_common.py
@@ -326,3 +326,64 @@ def test_change_scaling():
     assert np.all(las.X == [0, 2, 4, 6])
     assert np.all(las.Y == [-190, -180, -170, -160])
     assert np.all(las.Z == [-2900, -2800, -2700, -2600])
+
+
+def test_setting_x_y_z_on_las_data():
+    """
+    The goal of this test if to make sure that when setting the `x`,`y` and `z`
+    attribute of a LasData object, the X,Y,Z version of the coordinates
+    are properly set in the inner point record
+    """
+    las = laspy.read(simple_las)
+
+    new_las = laspy.create()
+
+    new_las.x = las.x
+    new_las.y = las.y
+    new_las.z = las.z
+
+    assert np.all(new_las.x == las.x)
+    assert np.all(new_las.X == las.X)
+    assert np.all(new_las.y == las.y)
+    assert np.all(new_las.Y == las.Y)
+    assert np.all(new_las.z == las.z)
+    assert np.all(new_las.Z == las.Z)
+
+
+    new_las = laspy.lib.write_then_read_again(new_las)
+
+    assert np.all(new_las.x == las.x)
+    assert np.all(new_las.X == las.X)
+    assert np.all(new_las.y == las.y)
+    assert np.all(new_las.Y == las.Y)
+    assert np.all(new_las.z == las.z)
+    assert np.all(new_las.Z == las.Z)
+
+
+def test_setting_xyz_on_las_data():
+    """
+    The goal of this test if to make sure that when setting the `xyz`
+    attribute of a LasData object, the X,Y,Z version of the coordinates
+    are properly set in the inner point record
+    """
+    las = laspy.read(simple_las)
+
+    new_las = laspy.create()
+
+    new_las.xyz = las.xyz
+
+    assert np.all(new_las.x == las.x)
+    assert np.all(new_las.X == las.X)
+    assert np.all(new_las.y == las.y)
+    assert np.all(new_las.Y == las.Y)
+    assert np.all(new_las.z == las.z)
+    assert np.all(new_las.Z == las.Z)
+
+    new_las = laspy.lib.write_then_read_again(new_las)
+
+    assert np.all(new_las.x == las.x)
+    assert np.all(new_las.X == las.X)
+    assert np.all(new_las.y == las.y)
+    assert np.all(new_las.Y == las.Y)
+    assert np.all(new_las.z == las.z)
+    assert np.all(new_las.Z == las.Z)