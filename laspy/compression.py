88b2640f5502cb6241fb1a83792fd72f3d753d82
tmontaigu <thomas.montaigu@laposte.net>
2026-04-01 21:14:31 +0200

    Use explicit impors instead of glob

--- laspy/compression.py
+++ laspy/compression.py
@@ -1,7 +1,12 @@
 """The functions related to the LAZ format (compressed LAS)"""
 
-from ._compression.backend import *
-from ._compression.format import *
+from ._compression.backend import LazBackend
+from ._compression.format import (
+    compressed_id_to_uncompressed,
+    is_point_format_compressed,
+    uncompressed_id_to_compressed,
+)
+from ._compression.selection import DecompressionSelection
 
 __all__ = [
     "LazBackend",
