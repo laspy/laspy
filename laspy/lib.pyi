a7e3dd308fa37e9ca0cd57ce40a60f66b44530fa
tmontaigu <thomas.montaigu@laposte.net>
2026-04-03 23:38:58 +0200

    fix? a few typing

--- laspy/lib.pyi
+++ laspy/lib.pyi
@@ -2,7 +2,7 @@ from typing import BinaryIO, Iterable, Literal, overload
 
 from . import LasWriter, PointFormat
 from .compression import DecompressionSelection, LazBackend
-from .header import LasHeader
+from .header import LasHeader, Version
 from .lasappender import LasAppender
 from .lasdata import LasData
 from .lasmmap import LasMMAP
@@ -14,10 +14,12 @@ DecompressionSelection = DecompressionSelection
 
 @overload
 def open_las(
-    source: BinaryIO | PathLike,
+    source: BinaryIO | PathLike | bytes,
     mode: Literal["r"] = ...,
     closefd: bool = ...,
     laz_backend: LazBackend | Iterable[LazBackend] = ...,
+    read_evlrs: bool = ...,
+    decompression_selection: DecompressionSelection = ...,
     fullwave: Literal["never", "lazy", "eager"] = ...,
 ) -> LasReader: ...
 @overload
@@ -25,14 +27,18 @@ def open_las(
     source: PathLike,
     mode: Literal["r"] = ...,
     laz_backend: LazBackend | Iterable[LazBackend] = ...,
+    read_evlrs: bool = ...,
+    decompression_selection: DecompressionSelection = ...,
     fullwave: Literal["never", "lazy", "eager"] = ...,
 ) -> LasReader: ...
 @overload
 def open_las(
-    source: BinaryIO,
+    source: BinaryIO | bytes,
     mode: Literal["r"] = ...,
     closefd: bool = ...,
     laz_backend: LazBackend | Iterable[LazBackend] = ...,
+    read_evlrs: bool = ...,
+    decompression_selection: DecompressionSelection = ...,
     fullwave: Literal["never", "lazy", "eager"] = ...,
 ) -> LasReader: ...
 @overload
@@ -42,16 +48,18 @@ def open_las(
     header: LasHeader,
     do_compress: bool | None = ...,
     laz_backend: LazBackend | Iterable[LazBackend] = ...,
+    encoding_errors: str = ...,
     fullwave: Literal["never", "lazy", "eager"] = ...,
 ) -> LasWriter: ...
 @overload
 def open_las(
-    source: BinaryIO,
+    source: BinaryIO | bytes,
     mode: Literal["w"],
     header: LasHeader,
     do_compress: bool | None = ...,
     closefd: bool = ...,
     laz_backend: LazBackend | Iterable[LazBackend] = ...,
+    encoding_errors: str = ...,
     fullwave: Literal["never", "lazy", "eager"] = ...,
 ) -> LasWriter: ...
 @overload
@@ -59,27 +67,28 @@ def open_las(
     source: PathLike,
     mode: Literal["a"],
     laz_backend: LazBackend | Iterable[LazBackend] = ...,
+    encoding_errors: str = ...,
     fullwave: Literal["never", "lazy", "eager"] = ...,
 ) -> LasAppender: ...
 @overload
 def open_las(
-    source: BinaryIO,
+    source: BinaryIO | bytes,
     mode: Literal["a"],
     closefd: bool = ...,
     laz_backend: LazBackend | Iterable[LazBackend] = ...,
+    encoding_errors: str = ...,
     fullwave: Literal["never", "lazy", "eager"] = ...,
 ) -> LasAppender: ...
 def read_las(
-    source: BinaryIO | PathLike,
+    source: BinaryIO | PathLike | bytes,
     closefd: bool = True,
     laz_backend: LazBackend | Iterable[LazBackend] = LazBackend.detect_available(),
     decompression_selection: DecompressionSelection = DecompressionSelection.all(),
     fullwave: Literal["never", "lazy", "eager"] = ...,
 ) -> LasData: ...
 def mmap_las(filename: PathLike) -> LasMMAP: ...
-def merge_las(las_files: Iterable[LasData] | LasData) -> LasData: ...
 def create_las(
-    *, point_format: int | PointFormat = 0, file_version: str | None = 0
+    *, point_format: int | PointFormat | None = None, file_version: str | Version | None = None
 ) -> LasData: ...
 def convert(
     source_las: LasData,
@@ -87,7 +96,8 @@ def convert(
     point_format_id: int | None = ...,
     file_version: str | None = ...,
 ) -> LasData: ...
-def create_from_header(header: LasHeader) -> LasData: ...
 def write_then_read_again(
-    las: LasData, do_compress: bool = ..., laz_backend: LazBackend = ...
+    las: LasData,
+    do_compress: bool = ...,
+    laz_backend: LazBackend | Iterable[LazBackend] = ...,
 ) -> LasData: ...
